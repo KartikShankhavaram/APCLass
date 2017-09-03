4e10187e14eff36001bcad93884f2c9117c9fbce
Kartik Shankhavaram <shankhavaramk@gmail.com>
2017-09-04 00:15:52 +0530

    added homework 2

---
+++ src/com/kartik/ClassProjectMall/src/com/kartik/Employees.java
@@ -0,0 +1,11 @@
+package com.kartik;
+
+public class Employees {
+
+    protected static double baseSalary = 40000d;
+    protected static double hourlyRate = 1000d;
+    protected static float overtimeRate = 500f;
+    protected static float percentageSales = 5;
+
+
+}
---
+++ src/com/kartik/ClassProjectMall/src/com/kartik/SalariedEmployees.java
@@ -0,0 +1,9 @@
+package com.kartik;
+
+public class SalariedEmployees extends Employees{
+
+    public double getSalary() {
+        return baseSalary;
+    }
+
+}
