ae5d5441f5ce107171b32ec02eca577c085c6776
Kartik Shankhavaram <shankhavaramk@gmail.com>
2017-09-03 23:58:57 +0530

    added homework 1

---
+++ src/com/kartik/ClassProject/src/com/kartik/Membership.java
@@ -0,0 +1,12 @@
+package com.kartik;
+
+public class Membership {
+
+    // For service
+    protected static int premiumDiscountPercent = 20;
+    protected static int goldDiscountPercent = 15;
+    protected static int silverDiscountPercent = 10;
+
+    // For product
+    protected static int memberProductDiscount = 10;
+}
